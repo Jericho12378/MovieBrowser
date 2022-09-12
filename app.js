13f6d47899aaad872111189ace0060f542552c65
Jericho Ferriol <jericho12378@gmail.com>
2022-09-12 14:13:06 +0800

    Browse Movies now Available

---
+++ app.js
@@ -0,0 +1,55 @@
+// https://www.omdbapi.com/?apikey=aca2f055&s=movie&type=movie&y=2020
+//<i class="fa-solid fa-film"></i>
+//https://www.omdbapi.com/?apikey=aca2f055&s=movie&y=?
+// aca2f055
+
+const movieLists = document.querySelector(".movies");
+let movieTitle = "Movie";
+
+async function getMovies() {
+
+  return moviesDat;
+}
+
+async function searchMovie() {
+   movieTitle = document.getElementById("inputfield").value;
+  let movies = await fetch(`https://www.omdbapi.com/?apikey=aca2f055&s=${movieTitle}&type=movie&y=?`);
+  let moviesData = await movies.json();
+  console.log(moviesData)
+  movieLists.innerHTML = moviesData.Search.map((element) => postHTML(element)).join("");
+}
+
+async function renderMovies(filter) {
+  const movies = await fetch(`https://www.omdbapi.com/?apikey=aca2f055&s=${movieTitle}&type=movie&y=?`);
+  const moviesData = await movies.json();
+  
+  if (filter === "YearAscending") {
+    moviesData.Search.sort((a, b) => a.Year - b.Year);
+  } else if (filter === "YearDescending") {
+    moviesData.Search.sort((a, b) => b.Year - a.Year);
+  }
+  
+  movieLists.innerHTML = moviesData.Search.map((element) => postHTML(element)).join("");
+}
+
+function postHTML(element) {
+  return `<div class="movie">
+  <figure class="movie__image--wrapper">
+    <img class="movie__img" src="${element.Poster}" alt="">
+  </figure>
+  <div class="movie__title">
+     <h3>${element.Title}</h3>
+  </div>
+  <div class="movie__year">
+    <p>${element.Year}</p>
+  </div>
+</div>`;
+}
+
+setTimeout(() => {
+  renderMovies();
+});
+
+function filterBooks(event) {
+  renderMovies(event.target.value);
+}
